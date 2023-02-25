49f1237547bbf6bfe84db2bebdafe176ac47331e
GitHubVad <onwww@yandex.ru>
2023-02-25 19:04:23 +0300

    first commit

---
+++ 010_IndexSearch/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = { 1, 21, 13, 37, 4, 5, 16,  37, 8};
+
+int n = array.Length;
+int find = 37;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
